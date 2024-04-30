f1a9175d6911a08f92999ebaa62071a005050b7b
swelanauguste <swelanauguste@gmail.com>
2024-04-30 09:19:58 -0400

    adding general search

--- app/incoming/urls.py
+++ app/incoming/urls.py
@@ -5,6 +5,7 @@ from .views import (
     IncomingDetailView,
     IncomingListView,
     IncomingUpdateView,
+    search_view,
 )
 
 urlpatterns = [
@@ -12,4 +13,5 @@ urlpatterns = [
     path("detail/<int:pk>/", IncomingDetailView.as_view(), name="incoming-detail"),
     path("update/<int:pk>/", IncomingUpdateView.as_view(), name="incoming-update"),
     path("add-new/", IncomingCreateView.as_view(), name="incoming-create"),
+    path("search/", search_view, name="search-view"),
 ]
--- app/incoming/views.py
+++ app/incoming/views.py
@@ -1,8 +1,34 @@
 from django.db.models import Q
+from django.shortcuts import render
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
+from outgoing.models import Outgoing
 
 from .forms import IncomingForm
 from .models import Incoming
+
+
+def search_view(request):
+    query = request.GET.get("q")
+    if query:
+        outgoings = Outgoing.objects.filter(
+            Q(subject__icontains=query)
+            | Q(out_from__icontains=query)
+            | Q(to__icontains=query)
+            | Q(cc__icontains=query)
+            | Q(notes__icontains=query)
+            # Assuming action has a name field you want to search
+        ).distinct()
+        incomings = Incoming.objects.filter(
+            Q(subject__icontains=query)
+            | Q(rfrom__icontains=query)
+            | Q(to__icontains=query)
+            | Q(cc__icontains=query)
+            | Q(notes__icontains=query)
+            | Q(action__icontains=query)
+            # Assuming action has a name field you want to search
+        ).distinct()
+    context = {"query": query, "incomings": incomings, "outgoings": outgoings}
+    return render(request, "search_view.html", context)
 
 
 class IncomingUpdateView(UpdateView):
